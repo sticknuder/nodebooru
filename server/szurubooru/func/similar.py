737ac95d5473bdd27c2eb1fb51167d05f600f7fd
Hunternif <hunternif@gmail.com>
2021-09-01 22:38:03 +0100

    server: update similar post logic to yield more results

--- server/szurubooru/func/similar.py
+++ server/szurubooru/func/similar.py
@@ -14,7 +14,7 @@ def find_similar_posts(source_post: model.Post, limit: int) -> List[model.Post]:
     # This will help yield results quicker
     source_tags = sorted(source_post.tags, key=lambda t: t.post_count)
     source_tag_count = len(source_tags)
-    max_removals = ceil(source_tag_count / 2)  # remove at most 50% of tags
+    max_removals = source_tag_count - 1
 
     tags = source_tags
     for x in range(max_removals + 1):
